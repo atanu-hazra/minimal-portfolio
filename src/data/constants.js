a12960dcaeb3f70e33225acd6cbd8a34aab5ccbc
Atanu Hazra <161182348+atanu-hazra@users.noreply.github.com>
2025-08-24 08:02:00 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
 export const CONTACT_EMAIL = "imatanuhazra@gmail.com";
-export const AVATAR_URL = "https://res.cloudinary.com/dmfvcclh3/image/upload/v1755523430/headshot_o29et0.png"
+export const AVATAR_URL = "https://res.cloudinary.com/dmfvcclh3/image/upload/v1756002534/profile-photo-1.jpg"
 export const LINKEDIN_URL = "https://www.linkedin.com/in/atanu-hazra"
 export const GITHUB_URL = "https://github.com/atanu-hazra"
 export const RESUME_LINK = "https://drive.google.com/drive/folders/13-AAW5iviLn-YX1gRGp-cii4krNSlAWt"
