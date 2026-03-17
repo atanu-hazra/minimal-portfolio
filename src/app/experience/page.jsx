171458fb720c549c82e57fb7fc10d52f1de2d337
Atanu Hazra <161182348+atanu-hazra@users.noreply.github.com>
2026-03-17 09:29:05 +0530

    add experience (#2)

---
+++ src/app/experience/page.jsx
@@ -0,0 +1,11 @@
+import ExperienceSection from "@/components/Sections/ExperienceSection";
+
+const ExperiencePage= () => {
+    return (
+        <div className="mt-10">
+            <ExperienceSection />
+        </div>
+    );
+};
+
+export default ExperiencePage;
